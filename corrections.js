ac65a717863edcc029a7ac9eaa4c7e7df9534d2f
RomanTsvetik <85985444+RomanTsvetik@users.noreply.github.com>
2021-06-25 10:31:11 +0300

    Add files via upload

---
+++ corrections.js
@@ -0,0 +1,21 @@
+//task 1
+for (let num = 1; num <= 100; num++) {
+    console.log(num);
+}
+
+//task3
+for (let a = 0; a <= 100; a+= 2){
+    if (a % 2 == 0) {
+        console.log(a);
+    }
+}
+
+//task5
+const array = [1, 2, 3, 4, 5];
+let total = 0;
+
+for (let i = 0; i < array.length; i++){
+    total += Math.pow(array[i], 2);    
+}
+console.log(total);
+    
